a3d834a206922f5865ccba90d22e9c940326e5a0
xulf <xu_linfei@126.com>
2018-06-22 17:03:47 +0800

    EBOM页面添加修改操作及页面

--- src/main/resources/static/js/bom/ebom/ebomManage.js
+++ src/main/resources/static/js/bom/ebom/ebomManage.js
@@ -1,6 +1,4 @@
 var firstLoad = true;
-// $(document).ready(
-//     $("#ebomManage").click(function () {
 window.onload = function(){
         var projectPuid = $("#project", window.top.document).val();
         var $table = $("#ebomManageTable");
@@ -10,17 +8,8 @@ window.onload = function(){
             type: "GET",
             success: function (result) {
                 var column = [];
-                column.push({field: 'eBomPuid', title: 'puid'});
-                // column.push({field: 'ck', checkbox: true, Width: 50});
-                // column.push({field: '',
-                //     title: '序号',
-                //     formatter: function (value, row, index) {
-                //         return index+1;},
-                //     align:
-                //         'center',
-                //     valign:
-                //         'middle'
-                // });
+                // column.push({field: 'eBomPuid', title: 'puid'});
+                column.push({field: 'ck', checkbox: true});
                 var data = result.data;
                 var nameZh =data[0];
                 var nameEn = data[1];
@@ -43,8 +32,11 @@ window.onload = function(){
                     url: "ebom/getEBom/list?projectId="+projectPuid,
                     method: 'get',
                     dataType:'json',
-                    cache: false,
+                    //cache: false,
                     striped: true,                              //是否显示行间隔色
+                    clickToSelect:true,
+                    disabled : true,//设置是否可用
+                    checked : true,//设置选中
                     sidePagination: "server",           //分页方式：client客户端分页，server服务端分页（*）
                     height: $(window.parent.document).find("#wrapper").height(),
                     width: $(window).width(),
@@ -52,22 +44,90 @@ window.onload = function(){
                     pageNumber:1,                       //初始化加载第一页，默认第一页
                     pageSize: 20,                       //每页的记录行数（*）
                     pageList: [20, 50,100,200],        //可供选择的每页的行数（*）
-                    uniqueId: "puid",                     //每一行的唯一标识，一般为主键列
-                    showExport: true,
-                    exportDataType: 'all',
+                    //uniqueId: "puid",                     //每一行的唯一标识，一般为主键列
+                    //showExport: true,
+                    //exportDataType: 'all',
                     columns: column,
                     sortable: true,                     //是否启用排序
                     sortOrder: "asc",                   //排序方式
-                    striped: true, //是否显示行间隔色
                     search: true, //是否显示表格搜索，此搜索是客户端搜索，不会进服务端
                     showColumns: true, //是否显示所有的列
                     fixedColumns: true,
-                    fixedNumber:1,
+                    fixedNumber:2,
                     showToggle: true,                   //是否显示详细视图和列表视图的切换按钮
                     showRefresh: true,                  //是否显示刷新按钮
-                    minimumCountColumns:4
+                    //minimumCountColumns:4,
+                    toolbars: [
+                        {
+                            text: '添加',
+                            iconCls: 'glyphicon glyphicon-plus',
+                            handler: function () {
+                                window.Ewin.dialog({
+                                    title: "添加",
+                                    url: "ebom/addEbom?projectId=" + projectPuid,
+                                    gridId: "gridId",
+                                    width: 500,
+                                    height: 650
+                                })
+                            }
+                        },
+                        {
+                            text: '修改',
+                            iconCls: 'glyphicon glyphicon-pencil',
+                            handler: function () {
+                                var rows = $("#ebomManageTable").bootstrapTable('getSelections');
+                                //只能选一条
+                                if (rows.length != 1) {
+                                    window.Ewin.alert({message: '请选择一条需要修改的数据!'});
+                                    return false;
+                                }
+                                window.Ewin.dialog({
+                                    title: "修改",
+                                    url: "ebom/updateEbom?projectId="+projectPuid +"puid="+ rows[0].puid ,
+                                    gridId: "gridId",
+                                    width: 500,
+                                    height: 650
+                                });
+                            }
+                        },
+                        {
+                            text: '删除',
+                            iconCls: 'glyphicon glyphicon-remove',
+                            handler: function () {
+                                var rows = $table.bootstrapTable('getSelections');
+                                if (rows.length == 0) {
+                                    window.Ewin.alert({message: '请选择一条需要删除的数据!'});
+                                    return false;
+                                }
+                                window.Ewin.confirm({title: '提示', message: '是否要删除您所选择的记录？', width: 500}).on(function (e) {
+                                    if (e) {
+                                        $.ajax({
+                                            type: "POST",
+                                            //ajax需要添加打包名
+                                            url: "./cfg0/deleteByPuid",
+                                            data: JSON.stringify(rows),
+                                            contentType: "application/json",
+                                            success: function (result) {
+                                                if (result.status) {
+                                                    window.Ewin.alert({message: result.msg});
+                                                    //刷新，会重新申请数据库数据
+                                                }
+                                                else {
+                                                    window.Ewin.alert({message: "操作删除失败:" + result.msg});
+                                                }
+                                                $table.bootstrapTable("refresh");
+                                            },
+                                            error: function (info) {
+                                                window.Ewin.alert({message: "操作删除:" + info.status});
+                                            }
+                                        })
+                                    }
+                                });
+                            }
+                        }
+                    ],
                 });
-               $table.bootstrapTable('hideColumn', 'eBomPuid');
+                //$table.bootstrapTable('hideColumn','puid');
                $table.bootstrapTable('hideColumn', 'puid');
          }
      });
--- src/main/resources/static/js/bom/pbom/pbomManage.js
+++ src/main/resources/static/js/bom/pbom/pbomManage.js
@@ -3,21 +3,13 @@ window.onload=function() {
     var $table = $("#pbomManageTable");
     var column = [];
     var projectId=$("#project", window.top.document).val();
-    // $.ajax({
-    //     url: "pbom/getBomManage?project="+projectId,
-    //     type: "GET",
-    //     success: function (data) {
-    //         var xxx = data.data;
-    //         console.log(xxx);
-    //     }
-    // })
     $.ajax({
         url: "pbom/manage/title?project="+projectId,
         type: "GET",
         success: function (result) {
             var column = [];
             column.push({field: 'eBomPuid', title: 'puid'});
-            // column.push({field: 'ck', checkbox: true, Width: 50});
+            column.push({field: 'ck', checkbox: true, Width: 50});
             // column.push({
             //     field: '',
             //     title: '序号',
@@ -72,7 +64,76 @@ window.onload=function() {
                 fixedNumber:1,
                 showToggle: true,                   //是否显示详细视图和列表视图的切换按钮
                 showRefresh: true,                  //是否显示刷新按钮
-                minimumCountColumns:4
+                minimumCountColumns:4,
+                toolbars: [
+                    {
+                        text: '添加',
+                        iconCls: 'glyphicon glyphicon-plus',
+                        handler: function () {
+                            window.Ewin.dialog({
+                                title: "添加",
+                                url: "ebom/addEbom?projectId=" + projectPuid,
+                                gridId: "gridId",
+                                width: 500,
+                                height: 650
+                            })
+                        }
+                    },
+                    {
+                        text: '修改',
+                        iconCls: 'glyphicon glyphicon-pencil',
+                        handler: function () {
+                            var rows = $table.bootstrapTable('getSelections');
+                            //只能选一条
+                            if (rows.length != 1) {
+                                window.Ewin.alert({message: '请选择一条需要修改的数据!'});
+                                return false;
+                            }
+                            window.Ewin.dialog({
+                                title: "修改",
+                                url: "cfg0/modifyPage?projectId=" + rows[0].puid,
+                                gridId: "gridId",
+                                width: 400,
+                                height: 500
+                            });
+                        }
+                    },
+                    {
+                        text: '删除',
+                        iconCls: 'glyphicon glyphicon-remove',
+                        handler: function () {
+                            var rows = $table.bootstrapTable('getSelections');
+                            if (rows.length == 0) {
+                                window.Ewin.alert({message: '请选择一条需要删除的数据!'});
+                                return false;
+                            }
+                            window.Ewin.confirm({title: '提示', message: '是否要删除您所选择的记录？', width: 500}).on(function (e) {
+                                if (e) {
+                                    $.ajax({
+                                        type: "POST",
+                                        //ajax需要添加打包名
+                                        url: "./cfg0/deleteByPuid",
+                                        data: JSON.stringify(rows),
+                                        contentType: "application/json",
+                                        success: function (result) {
+                                            if (result.status) {
+                                                window.Ewin.alert({message: result.msg});
+                                                //刷新，会重新申请数据库数据
+                                            }
+                                            else {
+                                                window.Ewin.alert({message: "操作删除失败:" + result.msg});
+                                            }
+                                            $table.bootstrapTable("refresh");
+                                        },
+                                        error: function (info) {
+                                            window.Ewin.alert({message: "操作删除:" + info.status});
+                                        }
+                                    })
+                                }
+                            });
+                        }
+                    }
+                ],
             });
             $table.bootstrapTable('hideColumn', 'eBomPuid');
         }
