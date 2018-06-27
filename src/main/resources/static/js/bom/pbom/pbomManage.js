ebfbed93969973078a08a7fd58b3ff28a44f19bd
xulf <xu_linfei@126.com>
2018-06-27 13:57:57 +0800

    EBOM  PBOM  搜索框调试

--- src/main/resources/static/js/bom/pbom/pbomManage.js
+++ src/main/resources/static/js/bom/pbom/pbomManage.js
@@ -1,17 +1,30 @@
 $(document).ready((function () {
-    initTable();
+    var projectPuid = $("#project", window.top.document).val();
+    var pBomUrl ="pbom/getBomManage?projectId=" + projectPuid;
+    initTable(pBomUrl);
 }))
 
 function doQuery() {
-    $('#pbomManageTable').bootstrapTable('refresh');    //刷新表格
+    //$('#pbomManageTable').bootstrapTable('refresh');    //刷新表格
+    var projectPuid = $("#project", window.top.document).val();
+    var pBomUrl ="pbom/getBomManage?projectId=" + projectPuid;
+    var level = $("#level").val();
+    pBomUrl+="&level="+level;
+    var pBomOfWhichDept = $("#pBomOfWhichDept").val();
+    pBomUrl+="&pBomOfWhichDept="+pBomOfWhichDept;
+    var lineId = $("#lineId").val();
+    pBomUrl += "&lineId="+lineId;
+    initTable(pBomUrl);
+    $('#pbomManageTable').bootstrapTable('destroy');
+    console.log("有搜索框的参数是："+pBomUrl)
 }
 
-function initTable() {
+function initTable(pBomUrl) {
     var $table = $("#pbomManageTable");
     var column = [];
-    var projectId = $("#project", window.top.document).val();
+    var projectPuid = $("#project", window.top.document).val();
     $.ajax({
-        url: "pbom/manage/title?project=" + projectId,
+        url: "pbom/manage/title?project=" + projectPuid,
         type: "GET",
         success: function (result) {
             var column = [];
@@ -46,7 +59,7 @@ function initTable() {
                 }
             }
             $table.bootstrapTable({
-                url: "pbom/getBomManage?projectId=" + projectId,
+                url:pBomUrl,
                 method: 'GET',
                 dataType: 'json',
                 cache: false,
