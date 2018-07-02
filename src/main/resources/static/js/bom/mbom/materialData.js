06ace10d7470ca30426247b10a6a3c4ba934f0b0
xulf <xu_linfei@126.com>
2018-07-02 11:48:42 +0800

    物料数据增加添加页面和修改页面

--- src/main/resources/static/js/bom/mbom/materialData.js
+++ src/main/resources/static/js/bom/mbom/materialData.js
@@ -52,7 +52,7 @@ function initTable() {
                 //sidePagination: "server",           //分页方式：client客户端分页，server服务端分页（*）
                 height: $(window.parent.document).find("#wrapper").height() - 180,
                 width: $(window).width(),
-                formId :"queryMaterialData",
+                formId: "queryMaterialData",
                 undefinedText: "",//当数据为 undefined 时显示的字符
                 pagination: true,
                 //pageNumber:1,                       //初始化加载第一页，默认第一页
@@ -73,6 +73,76 @@ function initTable() {
                 showToggle: false,                   //是否显示详细视图和列表视图的切换按钮
                 showRefresh: true,                  //是否显示刷新按钮
                 //minimumCountColumns: 4,
+                toolbars: [
+                    {
+                        text: '添加',
+                        iconCls: 'glyphicon glyphicon-plus',
+                        handler: function () {
+                            window.Ewin.dialog({
+                                title: "添加",
+                                url: "mbom/addMBom?projectId=" + projectPuid + "&eBomPuid=" + rows[0].eBomPuid,
+                                gridId: "gridId",
+                                width: 500,
+                                height: 650
+                            })
+                        }
+                    },
+                    {
+                        text: '修改',
+                        iconCls: 'glyphicon glyphicon-pencil',
+                        handler: function () {
+                            var rows = $table.bootstrapTable('getSelections');
+                            //只能选一条
+                            if (rows.length != 1) {
+                                window.Ewin.alert({message: '请选择一条需要修改的数据!'});
+                                return false;
+                            }
+                            window.Ewin.dialog({
+                                title: "修改",
+                                url: "mbom/updateMBom?projectId=" + projectPuid + "&eBomPuid=" + rows[0].eBomPuid,
+                                gridId: "gridId",
+                                width: 500,
+                                height: 650
+                            });
+                        }
+                    },
+                    {
+                        text: '删除',
+                        iconCls: 'glyphicon glyphicon-remove',
+                        handler: function () {
+                            var rows = $table.bootstrapTable('getSelections');
+                            if (rows.length == 0) {
+                                window.Ewin.alert({message: '请选择一条需要删除的数据!'});
+                                return false;
+                            }
+                            window.Ewin.confirm({title: '提示', message: '是否要删除您所选择的记录？', width: 500}).on(function (e) {
+                                if (e) {
+                                    $.ajax({
+                                        type: "POST",
+                                        //ajax需要添加打包名
+                                        url: "mbom/delete?eBomPuid=" + rows[0].eBomPuid,
+                                        //data: JSON.stringify(rows),
+                                        contentType: "application/json",
+                                        success: function (result) {
+                                            /*if (result.status) {
+                                                window.Ewin.alert({message: result.errMsg});
+                                                //刷新，会重新申请数据库数据
+                                            }
+                                            else {
+                                                window.Ewin.alert({message: ":" + result.errMsg});
+                                            }*/
+                                            window.Ewin.alert({message: result.errMsg});
+                                            $table.bootstrapTable("refresh");
+                                        },
+                                        error: function (info) {
+                                            window.Ewin.alert({message: "操作删除:" + info.status});
+                                        }
+                                    })
+                                }
+                            });
+                        }
+                    }
+                ],
             });
             $table.bootstrapTable('hideColumn', 'Puid');
         }
