a31434bd67bb8f30ad1801726a13dcd3794b510b
xulf <xu_linfei@126.com>
2018-07-26 19:41:34 +0800

    vwo表单开发完成

---
+++ src/main/java/com/connor/hozon/bom/resources/controller/change/vwo/vwoController.java
@@ -0,0 +1,28 @@
+package com.connor.hozon.bom.resources.controller.change.vwo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * \* User: xulf
+ * \* Date: 2018/7/24
+ * \* Time: 14:07
+ * \
+ */
+@Controller
+@RequestMapping(value = "/vwo")
+public class vwoController {
+
+
+
+    @RequestMapping(value = "vwoFromList" ,method = RequestMethod.GET)
+    public String getVwoFromList(){
+        return "changeManage/vwo/vwoBasicInformation";
+    }
+
+    @RequestMapping(value = "changeDescription" ,method = RequestMethod.GET)
+    public String getvwoChangeDescription(){
+        return "changeManage/vwo/vwoChangeDescription";
+    }
+}
