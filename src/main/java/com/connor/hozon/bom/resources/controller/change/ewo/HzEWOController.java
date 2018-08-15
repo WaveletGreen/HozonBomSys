0f4a8d0fda940811e40bd51360f821be6fc19504
haozt <zhit876@163.com>
2018-08-15 14:12:38 +0800

    影响分析 发起流程

--- src/main/java/com/connor/hozon/bom/resources/controller/change/ewo/HzEWOController.java
+++ src/main/java/com/connor/hozon/bom/resources/controller/change/ewo/HzEWOController.java
@@ -1,8 +1,10 @@
 package com.connor.hozon.bom.resources.controller.change.ewo;
 
+import com.alibaba.fastjson.JSONObject;
 import com.connor.hozon.bom.resources.controller.BaseController;
 import com.connor.hozon.bom.resources.dto.request.InitiatingProcessReqDTO;
 import com.connor.hozon.bom.resources.dto.response.HzEWOChangeFormRespDTO;
+import com.connor.hozon.bom.resources.dto.response.HzEbomRespDTO;
 import com.connor.hozon.bom.resources.dto.response.OperateResultMessageRespDTO;
 import com.connor.hozon.bom.resources.query.HzEWOChangeRecordQuery;
 import com.connor.hozon.bom.resources.query.HzEWOImpactReferenceQuery;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import sql.pojo.change.HzEWOImpactReference;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -56,21 +59,111 @@ public class HzEWOController extends BaseController {
     /**
      * 流程变更表单记录
      */
+//    @RequestMapping(value = "change/record",method = RequestMethod.GET)
+//    public void changeProcessHistory(HzEWOChangeRecordQuery query,HttpServletResponse response){
+//        if(query.getEwoNo() == null || query.getProjectId() == null
+//                || query.getProjectId() == "" || query.getEwoNo() == ""){
+//            writeAjaxJSONResponse(ResultMessageBuilder.build(false,"非法参数！"),response);
+//            return ;
+//        }
+//        List<HzEbomRespDTO> respDTOList = hzEWOService.getEWOChangeFormRecord(query);
+//        if(ListUtil.isEmpty(respDTOList)){
+//            writeAjaxJSONResponse(ResultMessageBuilder.build(false,"暂无数据！"),response);
+//            return ;
+//        }
+//        writeAjaxJSONResponse(ResultMessageBuilder.build(respDTOList),response);
+//    }
+
+
     @RequestMapping(value = "change/record",method = RequestMethod.GET)
-    public void changeProcessHistory(HzEWOChangeRecordQuery query,HttpServletResponse response){
+    @ResponseBody
+    public JSONObject changeProcessHistory(HzEWOChangeRecordQuery query){
         if(query.getEwoNo() == null || query.getProjectId() == null
                 || query.getProjectId() == "" || query.getEwoNo() == ""){
-            writeAjaxJSONResponse(ResultMessageBuilder.build(false,"非法参数！"),response);
-            return ;
+            return new JSONObject();
         }
-        List<HzEWOChangeFormRespDTO> respDTOList = hzEWOService.getEWOChangeFormRecord(query);
+        List<HzEbomRespDTO> respDTOList = hzEWOService.getEWOChangeFormRecord(query);
         if(ListUtil.isEmpty(respDTOList)){
-            writeAjaxJSONResponse(ResultMessageBuilder.build(false,"暂无数据！"),response);
-            return ;
+            return new JSONObject();
         }
-        writeAjaxJSONResponse(ResultMessageBuilder.build(respDTOList),response);
+        JSONObject jsonObject = new JSONObject();
+        List<JSONObject> list = new ArrayList<>();
+        respDTOList.forEach(hzEbomRespDTO -> {
+            JSONObject object = new JSONObject();
+            object.put("puid", hzEbomRespDTO.getPuid());
+            object.put("level", hzEbomRespDTO.getLevel());
+            object.put("rank", hzEbomRespDTO.getRank());
+            object.put("pBomOfWhichDept", hzEbomRespDTO.getpBomOfWhichDept());
+            object.put("lineId", hzEbomRespDTO.getLineId());
+            object.put("fna",hzEbomRespDTO.getFna());
+            object.put("lineNo",hzEbomRespDTO.getLineNo());
+            object.put("pBomLinePartName", hzEbomRespDTO.getpBomLinePartName());
+            object.put("pBomLinePartClass", hzEbomRespDTO.getpBomLinePartClass());
+            object.put("pBomLinePartEnName",hzEbomRespDTO.getpBomLinePartEnName());
+            object.put("pBomLinePartResource", hzEbomRespDTO.getpBomLinePartResource());
+            object.put("pFastener", hzEbomRespDTO.getpFastener());
+            if(Integer.valueOf(1).equals(hzEbomRespDTO.getP3cpartFlag())){
+                object.put("p3cpartFlag", "Y");
+            }else {
+                object.put("p3cpartFlag", "N");
+            }
+            if(Integer.valueOf(1).equals(hzEbomRespDTO.getpInOutSideFlag())){
+                object.put("pInOutSideFlag", "内饰件");
+            }else {
+                object.put("pInOutSideFlag", "外饰件");
+            }
+            object.put("pUpc",hzEbomRespDTO.getpUpc());
+            object.put("pFnaDesc", hzEbomRespDTO.getpFnaDesc());
+            object.put("pUnit", hzEbomRespDTO.getpUnit());
+            object.put("pPictureNo",hzEbomRespDTO.getpPictureNo());
+            object.put("pPictureSheet", hzEbomRespDTO.getpPictureSheet());
+            object.put("pMaterialHigh", hzEbomRespDTO.getpMaterialHigh());
+            object.put("pMaterial1",hzEbomRespDTO.getpMaterial1());
+            object.put("pMaterial2", hzEbomRespDTO.getpMaterial2());
+            object.put("pMaterial3", hzEbomRespDTO.getpMaterial3());
+            object.put("pDensity",hzEbomRespDTO.getpDensity());
+            object.put("pMaterialStandard", hzEbomRespDTO.getpMaterialStandard());
+            object.put("pSurfaceTreat", hzEbomRespDTO.getpSurfaceTreat());
+            object.put("pTextureColorNum",hzEbomRespDTO.getpTextureColorNum());
+            object.put("pManuProcess", hzEbomRespDTO.getpManuProcess());
+            object.put("pSymmetry", hzEbomRespDTO.getpSymmetry());
+            object.put("pImportance",hzEbomRespDTO.getpImportance());
+            if(Integer.valueOf(1).equals(hzEbomRespDTO.getpRegulationFlag())){
+                object.put("pRegulationFlag", "Y");
+            }else{
+                object.put("pRegulationFlag", "N");
+            }
+            object.put("pBwgBoxPart", hzEbomRespDTO.getpBwgBoxPart());
+            object.put("pDevelopType",hzEbomRespDTO.getpDevelopType());
+            object.put("pDataVersion", hzEbomRespDTO.getpDataVersion());
+            object.put("pTargetWeight", hzEbomRespDTO.getpTargetWeight());
+            object.put("pFeatureWeight",hzEbomRespDTO.getpFeatureWeight());
+            object.put("pActualWeight", hzEbomRespDTO.getpActualWeight());
+            object.put("pFastenerStandard", hzEbomRespDTO.getpFastenerStandard());
+            object.put("pFastenerLevel",hzEbomRespDTO.getpFastenerLevel());
+
+            object.put("pTorque", hzEbomRespDTO.getpTorque());
+            object.put("pDutyEngineer",hzEbomRespDTO.getpDutyEngineer());
+            object.put("pSupply", hzEbomRespDTO.getpSupply());
+            object.put("pSupplyCode", hzEbomRespDTO.getpSupplyCode());
+            object.put("pRemark",hzEbomRespDTO.getpRemark());
+            object.put("pRegulationCode", hzEbomRespDTO.getpRegulationCode());
+            object.put("number",hzEbomRespDTO.getNumber());
+            object.put("pBuyEngineer",hzEbomRespDTO.getpBuyEngineer());
+            object.put("status",hzEbomRespDTO.getStatus());
+            object.put("changeFlag",hzEbomRespDTO.getChangeFlag());
+            list.add(object);
+        });
+        jsonObject.put("result",list);
+        return jsonObject;
     }
 
+
+    /**
+     * 影响分析
+     * @param query
+     * @param response
+     */
     @RequestMapping(value = "impact/reference",method = RequestMethod.GET)
     public void impactReferenceRecord(HzEWOImpactReferenceQuery query, HttpServletResponse response){
         if(query.getEwoNo() == null || query.getProjectId() == null
--- src/main/java/com/connor/hozon/bom/resources/service/change/impl/HzEWOServiceImpl.java
+++ src/main/java/com/connor/hozon/bom/resources/service/change/impl/HzEWOServiceImpl.java
@@ -4,6 +4,7 @@ import com.connor.hozon.bom.bomSystem.dao.impl.bom.HzBomLineRecordDaoImpl;
 import com.connor.hozon.bom.common.util.user.UserInfo;
 import com.connor.hozon.bom.resources.dto.request.InitiatingProcessReqDTO;
 import com.connor.hozon.bom.resources.dto.response.HzEWOChangeFormRespDTO;
+import com.connor.hozon.bom.resources.dto.response.HzEbomRespDTO;
 import com.connor.hozon.bom.resources.dto.response.OperateResultMessageRespDTO;
 import com.connor.hozon.bom.resources.mybatis.bom.HzEbomRecordDAO;
 import com.connor.hozon.bom.resources.mybatis.change.HzEWOBasicInfoDAO;
@@ -24,6 +25,8 @@ import sql.pojo.change.HzEWOBasicInfo;
 import sql.pojo.epl.HzEPLManageRecord;
 
 import java.util.*;
+
+import static com.connor.hozon.bom.resources.service.bom.impl.HzPbomServiceImpl.getLevelAndRank;
 
 /**
  * @Author: haozt
@@ -162,18 +165,18 @@ public class HzEWOServiceImpl implements HzEWOService {
     }
 
     @Override
-    public List<HzEWOChangeFormRespDTO> getEWOChangeFormRecord(HzEWOChangeRecordQuery query) {
+    public List<HzEbomRespDTO> getEWOChangeFormRecord(HzEWOChangeRecordQuery query) {
         try {
-            List<HzEWOChangeFormRespDTO> changeFormRespDTOList = new ArrayList<>();
+            List<HzEbomRespDTO> changeFormRespDTOList = new ArrayList<>();
+
             Map<String,Object> map = new HashMap<>();
             map.put("ewoNo",query.getEwoNo());
             map.put("projectId",query.getProjectId());
             List<HzBomLineRecord> changeRecords = hzBomLineRecordDao.findBomListForChange(map);
+            Set<HzBomLineRecord> set = new HashSet<>(changeRecords);
+            changeRecords = new ArrayList<>(set);
             if(ListUtil.isNotEmpty(changeRecords)){
                 for(HzBomLineRecord hzBomLineRecord :changeRecords){
-                    HzEWOChangeFormRespDTO respDTO = new HzEWOChangeFormRespDTO();
-                    respDTO.setPuid(hzBomLineRecord.getPuid());
-                    respDTO.setLineId(hzBomLineRecord.getLineID());
                     Map<String,Object> beforeMap = new HashMap<>();
                     beforeMap.put("projectId",query.getProjectId());
                     beforeMap.put("puid",hzBomLineRecord.getPuid());
@@ -188,12 +191,15 @@ public class HzEWOServiceImpl implements HzEWOService {
                     HzBomLineRecord beforeRecord = hzBomLineRecordDao.findBomLineByPuid(beforeMap);
                     HzBomLineRecord afterRecord = hzBomLineRecordDao.findBomLineByPuid(afterMap);
                     if(beforeRecord !=null){
-                        respDTO.setBefore(beforeRecord);
+                        HzEbomRespDTO respBeforeDTO = recordToRespDTO(beforeRecord);
+                        respBeforeDTO.setChangeFlag(0);
+                        changeFormRespDTOList.add(respBeforeDTO);
                     }
                     if(afterRecord != null){
-                        respDTO.setAfter(afterRecord);
+                        HzEbomRespDTO respAfterDTO = recordToRespDTO(afterRecord);
+                        respAfterDTO.setChangeFlag(1);
+                        changeFormRespDTOList.add(respAfterDTO);
                     }
-                    changeFormRespDTOList.add(respDTO);
                 }
             }
             return changeFormRespDTOList;
@@ -228,6 +234,76 @@ public class HzEWOServiceImpl implements HzEWOService {
         return lastFourIndex;
     }
 
+
+    private HzEbomRespDTO recordToRespDTO(HzBomLineRecord record){
+        HzEbomRespDTO  respDTO = new HzEbomRespDTO();
+        Integer is2Y = record.getIs2Y();
+        Integer hasChildren = record.getIsHas();
+        String lineIndex = record.getLineIndex();
+        String[] strings = getLevelAndRank(lineIndex, is2Y, hasChildren);
+        respDTO.setLevel(strings[0]);
+        respDTO.setRank(strings[1]);
+        respDTO.setLineNo(strings[2]);
+        respDTO.setpBomOfWhichDept(record.getpBomOfWhichDept());
+        respDTO.setLineId(record.getLineID());
+        respDTO.setFna(record.getpFnaInfo());
+        respDTO.setpBomLinePartClass(record.getpBomLinePartClass());
+        respDTO.setpBomLinePartEnName(record.getpBomLinePartEnName());
+        respDTO.setpBomLinePartResource(record.getpBomLinePartResource());
+        respDTO.setpBomLinePartName(record.getpBomLinePartName());
+        respDTO.setpFastener(record.getpFastener());
+     
+        if(Integer.valueOf(1).equals(record.getP3cpartFlag())){
+            respDTO.setP3cpartFlag( "Y");
+        }else {
+            respDTO.setP3cpartFlag( "N");
+        }
+        if(Integer.valueOf(1).equals(record.getpInOutSideFlag())){
+            respDTO.setpInOutSideFlag( "内饰件");
+        }else {
+            respDTO.setpInOutSideFlag( "外饰件");
+        }
+        respDTO.setpUpc(record.getpUpc());
+        respDTO.setpFnaDesc( record.getpFnaDesc());
+        respDTO.setpUnit( record.getpUnit());
+        respDTO.setpPictureNo(record.getpPictureNo());
+        respDTO.setpPictureSheet( record.getpPictureSheet());
+        respDTO.setpMaterialHigh( record.getpMaterialHigh());
+        respDTO.setpMaterial1(record.getpMaterial1());
+        respDTO.setpMaterial2( record.getpMaterial2());
+        respDTO.setpMaterial3( record.getpMaterial3());
+        respDTO.setpDensity(record.getpDensity());
+        respDTO.setpMaterialStandard( record.getpMaterialStandard());
+        respDTO.setpSurfaceTreat( record.getpSurfaceTreat());
+        respDTO.setpTextureColorNum(record.getpTextureColorNum());
+        respDTO.setpManuProcess( record.getpManuProcess());
+        respDTO.setpSymmetry( record.getpSymmetry());
+        respDTO.setpImportance(record.getpImportance());
+        if(Integer.valueOf(1).equals(record.getpRegulationFlag())){
+            respDTO.setpRegulationFlag( "Y");
+        }else{
+            respDTO.setpRegulationFlag( "N");
+        }
+        respDTO.setpBwgBoxPart( record.getpBwgBoxPart());
+        respDTO.setpDevelopType(record.getpDevelopType());
+        respDTO.setpDataVersion( record.getpDataVersion());
+        respDTO.setpTargetWeight( record.getpTargetWeight());
+        respDTO.setpFeatureWeight(record.getpFeatureWeight());
+        respDTO.setpActualWeight( record.getpActualWeight());
+        respDTO.setpFastenerStandard( record.getpFastenerStandard());
+        respDTO.setpFastenerLevel(record.getpFastenerLevel());
+
+        respDTO.setpTorque( record.getpTorque());
+        respDTO.setpDutyEngineer(record.getpDutyEngineer());
+        respDTO.setpSupply( record.getpSupply());
+        respDTO.setpSupplyCode( record.getpSupplyCode());
+        respDTO.setpRemark(record.getpRemark());
+        respDTO.setpRegulationCode( record.getpRegulationCode());
+        respDTO.setNumber(record.getNumber());
+        respDTO.setpBuyEngineer(record.getpBuyEngineer());
+        respDTO.setStatus(record.getStatus());
+        return respDTO;
+    }
 
 
 }
