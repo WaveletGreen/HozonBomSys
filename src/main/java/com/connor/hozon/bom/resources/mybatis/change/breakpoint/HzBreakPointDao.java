90b7c6c5637428e75c4629b0cb9916156d3c7ebb
Fancyear <1243093769@qq.com>
2018-09-03 14:17:39 +0800

    断点信息接口dao层

--- src/main/java/com/connor/hozon/bom/resources/mybatis/change/breakpoint/HzBreakPointDao.java
+++ src/main/java/com/connor/hozon/bom/resources/mybatis/change/breakpoint/HzBreakPointDao.java
@@ -4,7 +4,14 @@ import com.connor.hozon.bom.bomSystem.dao.BasicDao;
 import org.springframework.context.annotation.Configuration;
 import sql.pojo.integration.HzBreakPoint;
 
+import java.util.List;
+
 @Configuration
-public interface HzBreakPointDao extends BasicDao<HzBreakPoint>{
+public interface HzBreakPointDao extends BasicDao<HzBreakPoint> {
+    /**
+     * 查找所有断点信息数据
+     * @return
+     */
+    List<HzBreakPoint> selectAll();
 
 }
--- src/main/java/com/connor/hozon/bom/resources/mybatis/change/impl/HzBreakPointDaoImpl.java
+++ src/main/java/com/connor/hozon/bom/resources/mybatis/change/impl/HzBreakPointDaoImpl.java
@@ -5,6 +5,8 @@ import com.connor.hozon.bom.resources.mybatis.change.breakpoint.HzBreakPointDao;
 import org.springframework.context.annotation.Configuration;
 import sql.pojo.integration.HzBreakPoint;
 
+import java.util.List;
+
 /**
  * @Author: Fancyears·Maylos·Maywas
  * @Description:
@@ -13,7 +15,19 @@ import sql.pojo.integration.HzBreakPoint;
  */
 @Configuration
 public class HzBreakPointDaoImpl extends BasicDaoImpl<HzBreakPoint> implements HzBreakPointDao {
+    private final static HzBreakPoint POINT = new HzBreakPoint();
+
     public HzBreakPointDaoImpl() {
         clz = HzBreakPointDao.class;
+    }
+
+    /**
+     * 查找所有断点信息数据
+     *
+     * @return
+     */
+    @Override
+    public List<HzBreakPoint> selectAll() {
+        return baseSQLUtil.executeQuery(POINT, clz.getCanonicalName() + ".selectAll");
     }
 }
