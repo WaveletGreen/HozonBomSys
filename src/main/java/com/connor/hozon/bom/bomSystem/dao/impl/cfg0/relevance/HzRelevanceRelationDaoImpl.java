e77d43250d284862298a8f8b7147b3ec6aaace63
zhudb <a494548025@qq.com>
2018-09-05 16:22:30 +0800

    相关性向数据库添加前

--- src/main/java/com/connor/hozon/bom/bomSystem/dao/impl/cfg0/relevance/HzRelevanceRelationDaoImpl.java
+++ src/main/java/com/connor/hozon/bom/bomSystem/dao/impl/cfg0/relevance/HzRelevanceRelationDaoImpl.java
@@ -2,6 +2,7 @@ package com.connor.hozon.bom.bomSystem.dao.impl.cfg0.relevance;
 
 import com.connor.hozon.bom.bomSystem.dao.BasicDaoImpl;
 import com.connor.hozon.bom.bomSystem.dao.cfg.relevance.HzRelevanceRelationDao;
+import org.springframework.stereotype.Service;
 import sql.pojo.cfg.relevance.HzRelevanceRelation;
 
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
  * @Date: Created in 2018/9/5 13:52
  * @Modified By:
  */
+@Service
 public class HzRelevanceRelationDaoImpl extends BasicDaoImpl<HzRelevanceRelation> implements HzRelevanceRelationDao {
     private final static HzRelevanceRelation RELATION = new HzRelevanceRelation();
 
--- src/main/java/com/connor/hozon/bom/bomSystem/service/integrate/SynRelevanceService2.java
+++ src/main/java/com/connor/hozon/bom/bomSystem/service/integrate/SynRelevanceService2.java
@@ -1,6 +1,8 @@
 package com.connor.hozon.bom.bomSystem.service.integrate;
 
 import com.connor.hozon.bom.bomSystem.dao.cfg.HzColorModelDao;
+import com.connor.hozon.bom.bomSystem.dao.cfg.relevance.HzRelevanceBasicDao;
+import com.connor.hozon.bom.bomSystem.dao.cfg.relevance.HzRelevanceRelationDao;
 import com.connor.hozon.bom.bomSystem.service.cfg.HzCfg0ModelService;
 import com.connor.hozon.bom.bomSystem.service.cfg.HzCfg0Service;
 import com.connor.hozon.bom.common.base.entity.QueryBase;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import sql.pojo.cfg.HzCfg0Record;
 import sql.pojo.cfg.HzCfg0Relevance;
 import sql.pojo.cfg.HzColorModel2;
+import sql.pojo.cfg.relevance.HzRelevanceBasic;
 
 import java.util.*;
 
@@ -26,12 +29,19 @@ public class SynRelevanceService2 {
     //配色方案服务
     @Autowired
     private HzColorModelDao hzColorModelDao;
+    //相关性主表Dao
+    @Autowired
+    private HzRelevanceBasicDao hzRelevanceBasicDao;
+    //相关性关系表Dao
+    @Autowired
+    private HzRelevanceRelationDao hzRelevanceRelationDao;
 
     public JSONObject addRelevance(String projectPuid){
         JSONObject response = new JSONObject();
+        JSONArray datas = new JSONArray();
         Long index = 1L;
         //相关性list
-        List<HzCfg0Relevance> hzCfg0Relevances = new ArrayList<HzCfg0Relevance>();
+        List<HzRelevanceBasic> hzRelevanceBasics = new ArrayList<HzRelevanceBasic>();
         //相关性序号
 
         //搜索全部特性值，并经过P_CFG0_OBJECT_ID 升序排序
@@ -78,14 +88,21 @@ public class SynRelevanceService2 {
             Set<String> keys = colorMap.keySet();
             //生成相关性
             for(String key : keys){
-                HzCfg0Relevance hzCfg0Relevance = new HzCfg0Relevance();
-                hzCfg0Relevance.setpCfgUid(hzCfg0Record.getPuid());
-                hzCfg0Relevance.setpOptionfamilyName(hzCfg0Record.getpCfg0FamilyName());
-                hzCfg0Relevance.setpCfg0ObjectId(hzCfg0Record.getpCfg0ObjectId());
-                hzCfg0Relevance.setpCfg0Desc(hzCfg0Record.getpCfg0FamilyDesc());
-                hzCfg0Relevance.setColorCode(key);
-                hzCfg0Relevance.setColorDesc(colorMap.get(key).get(0).getColorName());
-                String relevanceCode = "$ROOT."+hzCfg0Relevance.getpOptionfamilyName()+" = '"+hzCfg0Relevance.getpCfg0ObjectId()+"' AND ";
+//                HzCfg0Relevance hzCfg0Relevance = new HzCfg0Relevance();
+//                hzCfg0Relevance.setpCfgUid(hzCfg0Record.getPuid());
+//                hzCfg0Relevance.setpOptionfamilyName(hzCfg0Record.getpCfg0FamilyName());
+//                hzCfg0Relevance.setpCfg0ObjectId(hzCfg0Record.getpCfg0ObjectId());
+//                hzCfg0Relevance.setpCfg0Desc(hzCfg0Record.getpCfg0FamilyDesc());
+//                hzCfg0Relevance.setColorCode(key);
+//                hzCfg0Relevance.setColorDesc(colorMap.get(key).get(0).getColorName());
+
+
+                //拼接相关性
+                String relevance = hzCfg0Record.getpCfg0FamilyName()+"-"+hzCfg0Record.getpCfg0ObjectId()+"-"+key;
+                //拼接相关性描述
+                String relevanceDesc = hzCfg0Record.getpCfg0FamilyDesc()+"-"+colorMap.get(key).get(0).getColorName();
+                //拼接相关性代码
+                String relevanceCode = "$ROOT."+hzCfg0Record.getpCfg0FamilyName()+" = '"+hzCfg0Record.getpCfg0ObjectId()+"' AND ";
                 List<HzColorModel2> hzColorModel2s1 = colorMap.get(key);
                 int size = hzColorModel2s1.size();
                 if(size==1){
@@ -101,23 +118,58 @@ public class SynRelevanceService2 {
                     }
                     relevanceCode+=" )";
                 }
-                hzCfg0Relevance.setCfg0Relevance(relevanceCode);
+//                hzCfg0Relevance.setCfg0Relevance(relevanceCode);
+//
+//                hzCfg0Relevances.add(hzCfg0Relevance);
 
-                hzCfg0Relevances.add(hzCfg0Relevance);
+                //后端添加
+                HzRelevanceBasic hzRelevanceBasic = new HzRelevanceBasic();
+                //颜色code
+                hzRelevanceBasic.setRbColorCode(key);
+                hzRelevanceBasic.setRbColorUid("12");
+                //特性code
+                hzRelevanceBasic.setRbFeatureCode(hzCfg0Record.getpCfg0FamilyName());
+                //特性id
+                hzRelevanceBasic.setRbFeatureUid(hzCfg0Record.getpCfg0FamilyPuid());
+                //特性值code
+                hzRelevanceBasic.setRbFeatureValueCode(hzCfg0Record.getpCfg0ObjectId());
+                //特性值id
+                hzRelevanceBasic.setRbFeatureValueUid(hzCfg0Record.getPuid());
+                //相关性
+                hzRelevanceBasic.setRbRelevance(relevance);
+                //相关性描述
+                hzRelevanceBasic.setRbRelevanceDesc(relevanceDesc);
+                //相关性代码
+                hzRelevanceBasic.setRbRelevanceCode(relevanceCode);
+                //项目代码
+                hzRelevanceBasic.setRbProjectUid(projectPuid);
+                //状态
+                hzRelevanceBasic.setRelevanceStatus(0);
+                hzRelevanceBasicDao.insertBasic(hzRelevanceBasic);
+
+
+                //前端显示
+                JSONObject data = new JSONObject();
+                data.put("index",index);
+                index++;
+                data.put("pOptionfamilyName",hzCfg0Record.getpCfg0FamilyName()+"-"+hzCfg0Record.getpCfg0ObjectId()+"-"+key);
+                data.put("pCfg0Desc",hzCfg0Record.getpCfg0FamilyDesc()+"-"+colorMap.get(key).get(0).getColorName());
+                data.put("cfg0Relevance",relevanceCode);
+                datas.add(data);
             }
 
         }
-        JSONArray datas = new JSONArray();
-        for(HzCfg0Relevance hzCfg0Relevance:hzCfg0Relevances){
-            JSONObject data = new JSONObject();
-            data.put("index",index);
-            index++;
-            data.put("pOptionfamilyName",hzCfg0Relevance.getpOptionfamilyName()+"-"+hzCfg0Relevance.getpCfg0ObjectId()+"-"+hzCfg0Relevance.getColorCode());
-            data.put("pCfg0Desc",hzCfg0Relevance.getpCfg0Desc()+"-"+hzCfg0Relevance.getColorDesc());
-            data.put("cfg0Relevance",hzCfg0Relevance.getCfg0Relevance());
-            datas.add(data);
-        }
-        response.put("totalCount",hzCfg0Relevances.size());
+
+//        for(HzCfg0Relevance hzCfg0Relevance:hzCfg0Relevances){
+//            JSONObject data = new JSONObject();
+//            data.put("index",index);
+//            index++;
+//            data.put("pOptionfamilyName",hzCfg0Relevance.getpOptionfamilyName()+"-"+hzCfg0Relevance.getpCfg0ObjectId()+"-"+hzCfg0Relevance.getColorCode());
+//            data.put("pCfg0Desc",hzCfg0Relevance.getpCfg0Desc()+"-"+hzCfg0Relevance.getColorDesc());
+//            data.put("cfg0Relevance",hzCfg0Relevance.getCfg0Relevance());
+//            datas.add(data);
+//        }
+        response.put("totalCount",index-1);
         response.put("result",datas);
         return response;
     }
