da243bff110de291ae73d5ebc79eb297aa99409b
xulf <xu_linfei@126.com>
2018-09-07 09:43:35 +0800

    BUG修改

--- src/main/java/com/connor/hozon/bom/resources/service/resourcesLibrary/dictionaryLibrary/impl/HzDictionaryLibraryServiceImpl.java
+++ src/main/java/com/connor/hozon/bom/resources/service/resourcesLibrary/dictionaryLibrary/impl/HzDictionaryLibraryServiceImpl.java
@@ -3,11 +3,11 @@ package com.connor.hozon.bom.resources.service.resourcesLibrary.dictionaryLibrar
 import com.connor.hozon.bom.resources.domain.dto.request.AddHzDictionaryLibraryReqDTO;
 import com.connor.hozon.bom.resources.domain.dto.response.HzDictionaryLibraryRespDTO;
 import com.connor.hozon.bom.resources.domain.dto.response.OperateResultMessageRespDTO;
+import com.connor.hozon.bom.resources.domain.model.HzDictionaryLibraryFactory;
 import com.connor.hozon.bom.resources.domain.query.HzDictionaryLibraryQuery;
 import com.connor.hozon.bom.resources.mybatis.resourcesLibrary.dictionaryLibrary.HzDictionaryLibraryDao;
 import com.connor.hozon.bom.resources.page.Page;
 import com.connor.hozon.bom.resources.service.resourcesLibrary.dictionaryLibrary.HzDictionaryLibraryService;
-import com.connor.hozon.bom.resources.util.DateUtil;
 import com.connor.hozon.bom.resources.util.PrivilegeUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -38,28 +38,7 @@ public class HzDictionaryLibraryServiceImpl implements HzDictionaryLibraryServic
             if(!b){
                 return OperateResultMessageRespDTO.getFailPrivilege();
             }
-            HzDictionaryLibrary library = new HzDictionaryLibrary();
-            library.setPuid(UUID.randomUUID().toString());
-            library.setProfessionCh(reqDTO.getProfessionCh());
-            library.setProfessionEn(reqDTO.getProfessionEn());
-            library.setClassificationCh(reqDTO.getClassificationCh());
-            library.setClassificationEn(reqDTO.getClassificationEn());
-            library.setGroupCode(reqDTO.getGroupCode());
-            library.setGroupCh(reqDTO.getGroupCh());
-            library.setGroupEn(reqDTO.getGroupEn());
-            library.setFamillyCode(reqDTO.getFamillyCode());
-            library.setFamillyCh(reqDTO.getFamillyCh());
-            library.setFamillyEn(reqDTO.getFamillyEn());
-            library.setEigenValue(reqDTO.getEigenValue());
-            library.setValueDescCh(reqDTO.getValueDescCh());
-            library.setValueDescEn(reqDTO.getValueDescEn());
-            library.setType(reqDTO.getType());
-            library.setValueSource(reqDTO.getValueSource());
-
-            library.setEffectTime(reqDTO.getEffectTime());
-            library.setFailureTime(reqDTO.getFailureTime());
-
-            library.setNote(reqDTO.getNote());
+            HzDictionaryLibrary library = HzDictionaryLibraryFactory.addDictionaryDTOHzDictionaryLibrary(reqDTO);
             int i = hzDictionaryLibraryDao.insert(library);
             if (i>0){
                 return OperateResultMessageRespDTO.getSuccessResult();
@@ -82,26 +61,7 @@ public class HzDictionaryLibraryServiceImpl implements HzDictionaryLibraryServic
             if(!b){
                 return OperateResultMessageRespDTO.getFailPrivilege();
             }
-            HzDictionaryLibrary library = new HzDictionaryLibrary();
-            library.setPuid(reqDTO.getPuid());
-            library.setProfessionCh(reqDTO.getProfessionCh());
-            library.setProfessionEn(reqDTO.getProfessionEn());
-            library.setClassificationCh(reqDTO.getClassificationCh());
-            library.setClassificationEn(reqDTO.getClassificationEn());
-            library.setGroupCode(reqDTO.getGroupCode());
-            library.setGroupCh(reqDTO.getGroupCh());
-            library.setGroupEn(reqDTO.getGroupEn());
-            library.setFamillyCode(reqDTO.getFamillyCode());
-            library.setFamillyCh(reqDTO.getFamillyCh());
-            library.setFamillyEn(reqDTO.getFamillyEn());
-            library.setEigenValue(reqDTO.getEigenValue());
-            library.setValueDescCh(reqDTO.getValueDescCh());
-            library.setValueDescEn(reqDTO.getValueDescEn());
-            library.setType(reqDTO.getType());
-            library.setValueSource(reqDTO.getValueSource());
-            library.setEffectTime(reqDTO.getEffectTime());
-            library.setFailureTime(reqDTO.getFailureTime());
-            library.setNote(reqDTO.getNote());
+            HzDictionaryLibrary library = HzDictionaryLibraryFactory.addDictionaryDTOHzDictionaryLibrary(reqDTO);
             int i = hzDictionaryLibraryDao.update(library);
             if (i>0){
                 return OperateResultMessageRespDTO.getSuccessResult();
@@ -128,28 +88,9 @@ public class HzDictionaryLibraryServiceImpl implements HzDictionaryLibraryServic
             List<HzDictionaryLibrary> list = libraries.getResult();
             List<HzDictionaryLibraryRespDTO>reqDTOList = new ArrayList<>();
             for (HzDictionaryLibrary library :list){
-                HzDictionaryLibraryRespDTO reqDTO = new HzDictionaryLibraryRespDTO();
-                reqDTO.setNo(++num);
-                reqDTO.setPuid(library.getPuid());
-                reqDTO.setProfessionCh(library.getProfessionCh());
-                reqDTO.setProfessionEn(library.getProfessionEn());
-                reqDTO.setClassificationCh(library.getClassificationCh());
-                reqDTO.setClassificationEn(library.getClassificationEn());
-                reqDTO.setGroupCode(library.getGroupCode());
-                reqDTO.setGroupCh(library.getGroupCh());
-                reqDTO.setGroupEn(library.getGroupEn());
-                reqDTO.setFamillyCode(library.getFamillyCode());
-                reqDTO.setFamillyCh(library.getFamillyCh());
-                reqDTO.setFamillyEn(library.getFamillyEn());
-                reqDTO.setEigenValue(library.getEigenValue());
-                reqDTO.setValueDescCh(library.getValueDescCh());
-                reqDTO.setValueDescEn(library.getValueDescEn());
-                reqDTO.setType(library.getType());
-                reqDTO.setValueSource(library.getValueSource());
-                reqDTO.setEffectTime(DateUtil.formatDirDate(library.getEffectTime()));
-                reqDTO.setFailureTime(DateUtil.formatDirDate(library.getFailureTime()));
-                reqDTO.setNote(library.getNote());
-                reqDTOList.add(reqDTO);
+                HzDictionaryLibraryRespDTO respDTO =HzDictionaryLibraryFactory.libraryToRespDTO(library);
+                respDTO.setNo(num++);
+                reqDTOList.add(respDTO);
             }
             return new Page<>(libraries.getPageNumber(),libraries.getPageSize(),libraries.getTotalCount(),reqDTOList);
         } catch (Exception e) {
@@ -167,27 +108,7 @@ public class HzDictionaryLibraryServiceImpl implements HzDictionaryLibraryServic
         HzDictionaryLibrary library = hzDictionaryLibraryDao.findDictionaryLibrary(puid);
         try {
             if (library !=null){
-                HzDictionaryLibraryRespDTO dto = new HzDictionaryLibraryRespDTO();
-                dto.setPuid(library.getPuid());
-                dto.setProfessionCh(library.getProfessionCh());
-                dto.setProfessionEn(library.getProfessionEn());
-                dto.setClassificationCh(library.getClassificationCh());
-                dto.setClassificationEn(library.getClassificationEn());
-                dto.setGroupCode(library.getGroupCode());
-                dto.setGroupCh(library.getGroupCh());
-                dto.setGroupEn(library.getGroupEn());
-                dto.setFamillyCode(library.getFamillyCode());
-                dto.setFamillyCh(library.getFamillyCh());
-                dto.setFamillyEn(library.getFamillyEn());
-                dto.setEigenValue(library.getEigenValue());
-                dto.setValueDescCh(library.getValueDescCh());
-                dto.setValueDescEn(library.getValueDescEn());
-                dto.setType(library.getType());
-                dto.setValueSource(library.getValueSource());
-                dto.setEffectTime(DateUtil.formatDirDate(library.getEffectTime()));
-                dto.setFailureTime(DateUtil.formatDirDate(library.getEffectTime()));
-                dto.setNote(library.getNote());
-                return dto;
+               return HzDictionaryLibraryFactory.libraryToRespDTO(library);
             }
         } catch (Exception e) {
             return null;
