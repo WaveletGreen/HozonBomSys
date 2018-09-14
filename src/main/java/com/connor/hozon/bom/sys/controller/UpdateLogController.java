9e17ca581cd644bf3d0e17d6cf32afbe85fb1557
xulf <xu_linfei@126.com>
2018-09-14 16:07:35 +0800

    完成更新日志手动添加html页面

--- src/main/java/com/connor/hozon/bom/sys/controller/UpdateLogController.java
+++ src/main/java/com/connor/hozon/bom/sys/controller/UpdateLogController.java
@@ -3,6 +3,7 @@ package com.connor.hozon.bom.sys.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,16 +15,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("updateLog")
 public class UpdateLogController {
 
-    @RequestMapping(value = "V1.1.1",method = RequestMethod.GET)
-    public String getUpdateLogPageVersion1() {
-        return "updateLog/V1.1.1";
-    }
-    @RequestMapping(value = "V1.1.2",method = RequestMethod.GET)
-    public String getUpdateLogPageVersion2(){
-        return "updateLog/V1.1.2";
-    }
-    @RequestMapping(value = "V1.1.3",method = RequestMethod.GET)
-    public String getUpdateLogPageVersion3(){
-        return "updateLog/V1.1.3";
+//    @RequestMapping(value = "V1.0.1",method = RequestMethod.GET)
+//    public String getUpdateLogPageVersion1() {
+//        return "updateLog/V1.0.1";
+//    }
+//    @RequestMapping(value = "V1.0.3",method = RequestMethod.GET)
+//    public String getUpdateLogPageVersion2(){
+//        return "updateLog/V1.0.3";
+//    }
+//    @RequestMapping(value = "V1.1.0",method = RequestMethod.GET)
+//    public String getUpdateLogPageVersion3(){
+//        return "updateLog/V1.1.0";
+//    }
+    @RequestMapping(value = "getUpdateLog",method = RequestMethod.GET)
+    public String getUpdateLog(@RequestParam("version") String version){
+        return "updateLog/"+version;
     }
 }
