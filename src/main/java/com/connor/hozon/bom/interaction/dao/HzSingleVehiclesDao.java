8feef12373f3a4b18d3c3ae317d203cf68518961
haozt <zhit876@163.com>
2018-09-26 10:10:43 +0800

    白车身和超级MBOM完成 最最终版

--- src/main/java/com/connor/hozon/bom/interaction/dao/HzSingleVehiclesDao.java
+++ src/main/java/com/connor/hozon/bom/interaction/dao/HzSingleVehiclesDao.java
@@ -37,5 +37,6 @@ public interface HzSingleVehiclesDao extends BasicDao<HzSingleVehicles> {
      */
     HzSingleVehicles selectByDmbIdWithProjectUid(Long svlDmbId, String projectUid);
 
+    int insert(HzSingleVehicles hzSingleVehicles);
 
 }
