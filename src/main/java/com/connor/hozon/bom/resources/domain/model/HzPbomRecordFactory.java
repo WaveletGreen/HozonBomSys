495737b3150e445907e87f1c44529d2917ac5170
haozt <zhit876@163.com>
2018-09-28 10:47:05 +0800

    导入EXCEL文件优化，单车BOM清单

--- src/main/java/com/connor/hozon/bom/resources/domain/model/HzPbomRecordFactory.java
+++ src/main/java/com/connor/hozon/bom/resources/domain/model/HzPbomRecordFactory.java
@@ -5,6 +5,7 @@ import com.connor.hozon.bom.common.util.user.UserInfo;
 import com.connor.hozon.bom.resources.domain.dto.request.AddHzEbomReqDTO;
 import com.connor.hozon.bom.resources.domain.dto.request.UpdateHzEbomReqDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import sql.pojo.bom.HzImportEbomRecord;
 import sql.pojo.bom.HzPbomLineRecord;
 
 import javax.xml.ws.soap.Addressing;
@@ -48,6 +49,30 @@ public class HzPbomRecordFactory {
         hzPbomLineRecord.setpBomLinePartClass(reqDTO.getpBomLinePartClass());
         hzPbomLineRecord.setLinePuid(UUID.randomUUID().toString());
         hzPbomLineRecord.setLineId(reqDTO.getLineId());
+        return hzPbomLineRecord;
+    }
+
+
+    public static HzPbomLineRecord ImportEbomRecordToPbomRecord(HzImportEbomRecord record){
+        HzPbomLineRecord hzPbomLineRecord = new HzPbomLineRecord();
+        hzPbomLineRecord.setUpdateName(UserInfo.getUser().getUserName());
+        hzPbomLineRecord.setCreateName(UserInfo.getUser().getUserName());
+        hzPbomLineRecord.setPuid(UUID.randomUUID().toString());
+        hzPbomLineRecord.setIsHas(record.getIsHas());
+        hzPbomLineRecord.setBomDigifaxId(record.getBomDigifaxId());
+        hzPbomLineRecord.seteBomPuid(record.getPuid());
+        hzPbomLineRecord.setLineId(record.getLineId());
+        hzPbomLineRecord.setIsPart(record.getIsPart());
+        hzPbomLineRecord.setIs2Y(record.getIs2Y());
+        hzPbomLineRecord.setLineIndex(record.getLineIndex());
+        hzPbomLineRecord.setParentUid(record.getParentId());
+        hzPbomLineRecord.setpBomLinePartClass(record.getpBomLinePartClass());
+        hzPbomLineRecord.setpBomLinePartName(record.getpBomLinePartName());
+        hzPbomLineRecord.setpBomOfWhichDept(record.getpBomOfWhichDept());
+        hzPbomLineRecord.setpBomLinePartEnName(record.getpBomLinePartEnName());
+        hzPbomLineRecord.setpBomLinePartResource(record.getpBomLinePartResource());
+        hzPbomLineRecord.setSortNum(record.getSortNum());
+        hzPbomLineRecord.setColorPart(record.getColorPart());
         return hzPbomLineRecord;
     }
 }
