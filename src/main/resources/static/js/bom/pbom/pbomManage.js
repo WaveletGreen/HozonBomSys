0305b93599c137082d028b2bec475923bf150d02
xulf <xu_linfei@126.com>
2018-10-12 09:26:52 +0800

    源码里分页增加跳转页码功能

--- src/main/resources/static/js/bom/pbom/pbomManage.js
+++ src/main/resources/static/js/bom/pbom/pbomManage.js
@@ -842,7 +842,12 @@ function queryLou(row) {
         }
     })
 }
-
+function toPage() {
+    var pageNum = $("#pageNum").val();
+    if (pageNum) {
+        $('#pbomManageTable').bootstrapTable('selectPage', parseInt(pageNum));
+    }
+}
 $(document).keydown(function (event) {
     if (event.keyCode == 13) {
         $('form').each(function () {
