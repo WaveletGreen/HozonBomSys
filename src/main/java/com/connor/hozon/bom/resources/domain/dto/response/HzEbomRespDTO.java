d95c6619ef61784fc5688d1785d3df2f7d6cfc9b
haozt <zhit876@163.com>
2018-10-17 16:36:33 +0800

    单车用量后端数据前端页面显示

--- src/main/java/com/connor/hozon/bom/resources/domain/dto/response/HzEbomRespDTO.java
+++ src/main/java/com/connor/hozon/bom/resources/domain/dto/response/HzEbomRespDTO.java
@@ -2,6 +2,8 @@ package com.connor.hozon.bom.resources.domain.dto.response;
 
 import com.alibaba.fastjson.JSONArray;
 
+import java.util.Map;
+
 /**
  * Created by haozt on 2018/06/06
  */
@@ -14,6 +16,19 @@ public class HzEbomRespDTO {
         return jsonArray;
     }
 
+    /**
+     * 显示单车用量
+     */
+    private Map<String,Object> map;
+
+    public Map<String, Object> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, Object> map) {
+        this.map = map;
+    }
+
     public void setJsonArray(JSONArray jsonArray) {
         this.jsonArray = jsonArray;
     }
@@ -22,10 +37,6 @@ public class HzEbomRespDTO {
 
     private String lineId;
     private String puid;
-    /**
-     * 单车用量
-     */
-    private String[] singleDosage;
 
     private String projectId;
     /**
@@ -158,14 +169,6 @@ public class HzEbomRespDTO {
     private Integer changeFlag;
 
     private String colorPart;
-
-    public String[] getSingleDosage() {
-        return singleDosage;
-    }
-
-    public void setSingleDosage(String[] singleDosage) {
-        this.singleDosage = singleDosage;
-    }
 
     public String getColorPart() {
         return colorPart;
