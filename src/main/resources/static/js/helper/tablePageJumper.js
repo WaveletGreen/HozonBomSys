3442742eefa059c0ddc22da374ffe584576445c8
Fancyear <1243093769@qq.com>
2018-11-06 21:18:24 +0800

    最后一次全部功能上传

--- src/main/resources/static/js/helper/tablePageJumper.js
+++ src/main/resources/static/js/helper/tablePageJumper.js
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2018.
- * This file was wrote by fancyears·milos·maywas @connor. Any question/bug you can post to 1243093366@qq.com.
+ * This file was wrote by fancyears·milos·malvis @connor. Any question/bug you can post to 1243093366@qq.com.
  * ALL RIGHTS RESERVED.
  */
 var $tableId = null;
