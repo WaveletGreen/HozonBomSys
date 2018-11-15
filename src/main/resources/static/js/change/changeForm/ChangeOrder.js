1ce6de96d62d9455ed9d441fe54cf2ba10a39d8f
zhudb <a494548025@qq.com>
2018-11-15 14:35:19 +0800

    提交

--- src/main/resources/static/js/change/changeForm/ChangeOrder.js
+++ src/main/resources/static/js/change/changeForm/ChangeOrder.js
@@ -58,6 +58,28 @@ var columnOfFeature1 = [
         }
     }];
 
+
+var columnOfModelColor = [
+    {
+        field: 'codeOfColorModel',
+        title: '车型颜色代码',
+        align: 'center',
+        valign: 'middle'
+    },
+    {
+        field: 'descOfColorModel',
+        title: '&emsp;&emsp;&emsp;&emsp;描述&emsp;&emsp;&emsp;&emsp;',
+        align: 'center',
+        valign: 'middle'
+    },
+    {
+        field: 'modelShell',
+        title: '油漆车身总成',
+        align: 'center',
+        valign: 'middle'
+    }
+];
+
 $(document).ready((function () {
 
 }))
@@ -96,5 +118,62 @@ function skip(type) {
                 changeTableRendering("routingDaTable");
             },
         });
+    }else if(type=="modelColor"){
+        $.ajax({
+            url: "../vwo/getModelColorTable?vwoId=" + 6,
+            type: "POST",
+            success: function (result) {
+                if (result) {
+                    initTable(result);
+                }
+            },
+            error: function (e) {
+                console.error("服务器错误:" + e.status);
+            }
+        })
     }
+}
+
+
+/**
+ * 初始化配色方案变更table
+ * @param data
+ */
+
+function initTable(data) {
+    let titles = data.titleSet;
+    let tableData = data.result;
+    for (let i in titles) {
+        columnOfModelColor.push({
+            field: "s" + i,
+            title:
+                titles[i],
+            align:
+                'center',
+            valign:
+                'middle'
+        })
+    }
+
+    let $table = $("#routingDaTable");
+    $table.bootstrapTable('destroy');
+    $table.bootstrapTable({
+        // url: "getModelColorTable?vwoId=" + vwoId,
+        method: "get",
+        data: tableData,
+        cache: false,
+        height: 400,// $(window.parent.document).find("#wrapper").height() - document.body.offsetHeight - 100,
+        width: $(window).width(),
+        showToggle: false,                   //是否显示详细视图和列表视图的切换按钮
+        showColumns: false,                  //是否显示所有的列
+        showRefresh: false,                  //是否显示刷新按钮
+        pagination: false,                   //是否显示分页（*）
+        clickToSelect: false,                // 单击某一行的时候选中某一条记录
+        formId: "null",                       //需要定义formId，不定义的话会造成jQuery异常
+        /**列信息，需要预先定义好*/
+        columns: columnOfModelColor,
+
+    });
+    //加载成功时执行,渲染双行的
+    changeTableRendering("routingDaTable");
 }
