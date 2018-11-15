a83b730b80974b13ac45890ac3081f9ac154fc0f
haozt <zhit876@163.com>
2018-11-15 11:08:48 +0800

    变更系列

--- src/main/java/com/connor/hozon/bom/resources/executors/ExecutorServices.java
+++ src/main/java/com/connor/hozon/bom/resources/executors/ExecutorServices.java
@@ -42,11 +42,8 @@ public  class ExecutorServices{
 
     public  void execute(){
         //execute 无返回值 无法判断线程的执行情况 submit方法 有返回值 可以判断每个线程执行结果
-        pool.execute(new Runnable() {
-            @Override
-            public void run() {
-                action();
-            }
+        pool.execute(()-> {
+            action();
         });
 
         if(pool != null){
