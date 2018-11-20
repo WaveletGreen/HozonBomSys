1282207c6f674e1a7078567a35403e79e324cca0
fancyears <1243093769@qq.com>
2018-11-20 17:18:58 +0800

    流程接口定义2

--- src/main/java/com/connor/hozon/bom/process/controller/ProcessController.java
+++ src/main/java/com/connor/hozon/bom/process/controller/ProcessController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 流程控制
  */
@@ -18,4 +21,27 @@ public class ProcessController {
     public String getAuditorPage() {
         return "change/changeForm/changeSelectAuditor";
     }
+
+    @RequestMapping("doCheck")
+    public Map<String, Object> doCheck(Map<String, String> params) {
+        Map<String, Object> result = new HashMap<>();
+        if (params == null || params.isEmpty() || !params.containsKey("agreement")) {
+            result.put("status", false);
+            result.put("msg", "入参无效");
+            return result;
+        }
+        switch (params.get("agreement")) {
+            case "1":
+                result.put("status", processManagerService.release(Long.parseLong(params.get("orderId"))));
+                break;
+            case "2":
+                result.put("status", processManagerService.interrupt(Long.parseLong(params.get("orderId"))));
+                break;
+            default:
+                result.put("status", false);
+                result.put("msg", "入参无效");
+        }
+        return result;
+    }
+
 }
--- src/main/java/com/connor/hozon/bom/process/service/ProcessManagerService.java
+++ src/main/java/com/connor/hozon/bom/process/service/ProcessManagerService.java
@@ -5,7 +5,6 @@ import com.connor.hozon.bom.bomSystem.service.process.InterruptionContainer;
 import com.connor.hozon.bom.bomSystem.service.process.ReleaseContainer;
 import com.connor.hozon.bom.process.iservice.IProcessManagerService;
 import net.sf.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
@@ -22,30 +21,51 @@ public class ProcessManagerService implements IProcessManagerService {
 
     @Override
     public Object start(long orderId, Object... param) {
-        return processStartEntity.doStart(orderId,param);
+        return processStartEntity.doStart(orderId, param);
     }
 
     @Override
     public Object release(long orderId, Object... param) {
         JSONObject result = new JSONObject();
-        boolean status = executeProcess(interruptEntity, new InterruptionContainer(), orderId, param);
+        boolean status = executeProcess(releaseEntity, new ReleaseContainer(), orderId, param);
         if (status) {
-            result.put("msg", "流程终止成功");
+            result.put("msg", "发布成功");
+            updateStatus(orderId, 999, true);
         } else {
-            result.put("msg", "流程终止失败");
+            result.put("msg", "发布失败");
         }
         result.put("status", status);
         return result;
     }
 
+    /**
+     * 更新任务状态和表单状态
+     *
+     * @param orderId
+     * @param i
+     * @param b
+     */
+    private void updateStatus(long orderId, int i, boolean b) {
+        updateTask(orderId, i);
+        updateOrder(orderId, i);
+    }
+
+    private void updateTask(long orderId, int i) {
+    }
+
+    private void updateOrder(long orderId, int i) {
+    }
+
+
     @Override
     public Object interrupt(long orderId, Object... param) {
         JSONObject result = new JSONObject();
-        boolean status = executeProcess(releaseEntity, new ReleaseContainer(), orderId, param);
+        boolean status = executeProcess(interruptEntity, new InterruptionContainer(), orderId, param);
         if (status) {
-            result.put("msg", "发布成功");
+            result.put("msg", "流程终止成功");
+            updateStatus(orderId, 900, true);
         } else {
-            result.put("msg", "发布失败");
+            result.put("msg", "流程终止失败");
         }
         result.put("status", status);
         return result;
