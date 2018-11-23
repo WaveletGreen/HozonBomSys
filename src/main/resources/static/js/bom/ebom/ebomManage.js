5fd87837b891e975eb239a635da61967c19f255f
xulf <xu_linfei@126.com>
2018-11-23 09:06:23 +0800

    發起流程時puids字符轉換成數組

--- src/main/resources/static/js/bom/ebom/ebomManage.js
+++ src/main/resources/static/js/bom/ebom/ebomManage.js
@@ -424,20 +424,15 @@ function initTable(eBomUrl) {
                         iconCls: 'glyphicon glyphicon-log-out',
                         handler: function () {
                             var rows = $table.bootstrapTable('getSelections');
-                            // var puids = "";
-                            // for (var i = 0; i < rows.length; i++) {
-                            //     puids += rows[i].puid + ",";
-                            // }
-                            // ;
-                            var puids = [];
-
-                            for (vari = 0; i < rows.length; i++) {
-                                puids.push(rows[i].puid)
+                            var puids = "";
+                            for (var i = 0; i < rows.length; i++) {
+                                puids += rows[i].puid + ",";
                             }
-                            var myData = JSON.stringify({
-                                "projectId": $("#project", window.top.document).val(),
-                                "puids": puids,
-                            });
+                            ;
+                            // var myData = JSON.stringify({
+                            //     "projectId": $("#project", window.top.document).val(),
+                            //     "puids": puids,
+                            // });
                             if (rows.length == 0) {
                                 window.Ewin.alert({message: '请选择需要变更的数据!'});
                                 return false;
@@ -452,9 +447,7 @@ function initTable(eBomUrl) {
                             }
                             window.Ewin.dialog({
                                 title: "选择变更表单",
-                                // url: "ebom/order/choose?projectId="+projectPuid+"&puids="+puids,
-                                url :"ebom/order/choose",
-                                data:myData,
+                                url: "ebom/order/choose?projectId="+projectPuid+"&puids="+puids,
                                 gridId: "gridId",
                                 width: 450,
                                 height: 450
