7deb3b636747bcf00f0c3abcea22bd109748b7db
zhudb <a494548025@qq.com>
2018-11-28 15:44:36 +0800

    为特性、配色方案、全配置BOM的删除判断了数据是否生效

--- src/main/java/com/connor/hozon/bom/bomSystem/service/vwo/HzVwoManagerService.java
+++ src/main/java/com/connor/hozon/bom/bomSystem/service/vwo/HzVwoManagerService.java
@@ -3616,7 +3616,7 @@ public JSONObject getVWO(List<HzCfg0ModelColor> colors, String projectPuid, Arra
         result.put("msg","删除成功");
         if(hzFullCfgMainDao.updateChangeByOrderId(orderId)<=0?true:false){
             result.put("status",false);
-            result.put("msg","修改源数据失败");
+            result.put("msg","修改源数据失败,请确定您删除的数据为未生效的数据");
             return result;
         }
         if(hzFullCfgMainChangeDao.deleteById(mainId)<=0?true:false){
