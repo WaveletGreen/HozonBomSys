99f75347ed1ea2276659484442b1c4d56583ac7c
xulf <xu_linfei@126.com>
2018-12-07 10:26:52 +0800

    断点信息显示数据的修改，前端新增序号显示

--- src/main/resources/static/js/change/breakPoint/breakPoint.js
+++ src/main/resources/static/js/change/breakPoint/breakPoint.js
@@ -1,3 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: xlf
+ * Date: 2018/12/7
+ * Time: 10:18
+ */
 $(document).ready(
     // $("#queryColorSet").click(function () {
     (function () {
@@ -10,17 +16,17 @@ function loadData() {
     $table.bootstrapTable('destroy');
     $table.bootstrapTable({
         url: "codpwo/codpwoFromList",
-        method:"GET",
+        method: "GET",
         height: $(window.parent.document).find("#wrapper").height() - 150,//$(window.parent.document).find("#wrapper").height() - document.body.offsetHeight-100,
         width: $(window).width(),
         showToggle: false,                   //是否显示详细视图和列表视图的切换按钮
         showRefresh: true,                  //是否显示刷新按钮
         pagination: true,                   //是否显示分页（*）
-        pageNumber:1,                       //初始化加载第一页，默认第一页
+        pageNumber: 1,                       //初始化加载第一页，默认第一页
         pageSize: 10,                       //每页的记录行数（*）
-        pageList: [10, 30,50,100,500,1000],//可供选择的每页的行数（*）
+        pageList: [10, 30, 50, 100, 500, 1000],//可供选择的每页的行数（*）
         clickToSelect: true,                // 单击某一行的时候选中某一条记录
-        smartDisplay:false,
+        smartDisplay: false,
         //search:true,                      //输入框“查询”
         sortable: true,
         sortName: 'pid',//一个对应字段
@@ -30,13 +36,24 @@ function loadData() {
 
         /**列信息，需要预先定义好*/
         columns: [
+            {
+                field: '',
+                title: '序号',
+                align: 'center',
+                width: 50,
+                formatter: function (value, row, index) {
+                    var options = $table.bootstrapTable('getOptions');
+                    return options.pageSize * (options.pageNumber - 1) + index + 1;
+
+                }
+            },
             // {
             //     field: 'ck',
             //     checkbox: true
             // },
-            {field: 'pid', title: '主键', sortable: true, sortOrder: 'asc',},
-            {field: 'pPackno', title: '数据包号', align: 'center', valign: 'middle', sortable: true, sortOrder: 'asc',},
-            {field: 'pItem', title: '行号', align: 'center', valign: 'middle', sortable: true, sortOrder: 'asc',},
+            // {field: 'pid', title: '主键', sortable: true, sortOrder: 'asc',},
+            // {field: 'pPackno', title: '数据包号', align: 'center', valign: 'middle', sortable: true, sortOrder: 'asc',},
+            // {field: 'pItem', title: '行号', align: 'center', valign: 'middle', sortable: true, sortOrder: 'asc',},
             {field: 'pTcecn', title: 'TC-ECN', align: 'center', valign: 'middle', sortable: true, sortOrder: 'asc',},
             {field: 'pEcn', title: 'SAP-ECN', align: 'center', valign: 'middle', sortable: true, sortOrder: 'asc',},
             {field: 'pDate', title: '断点日期', align: 'center', valign: 'middle', sortable: true, sortOrder: 'asc',},
@@ -46,7 +63,13 @@ function loadData() {
             {field: 'pUser', title: '断点执行工号', align: 'center', valign: 'middle', sortable: true, sortOrder: 'asc',},
             {field: 'pCdate', title: '断点处理日期', align: 'center', valign: 'middle', sortable: true, sortOrder: 'asc',},
             {field: 'pCtime', title: '断点处理时间', align: 'center', valign: 'middle', sortable: true, sortOrder: 'asc',},
-            {field: 'feedbackTime', title: '断点回传到BOM系统时间', align: 'center', valign: 'middle', sortable: true, sortOrder: 'asc',
+            {
+                field: 'feedbackTime',
+                title: '断点回传到BOM系统时间',
+                align: 'center',
+                valign: 'middle',
+                sortable: true,
+                sortOrder: 'asc',
                 //——修改——获取日期列的值进行转换
                 formatter: function (value, row, index) {
                     return changeDateFormat(value)
